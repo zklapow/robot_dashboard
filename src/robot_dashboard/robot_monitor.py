8be35e3a6ade17a19f53b94341e257e1aa38342b
Ze'ev Klapow <zklapow@willowgarage.com>
2012-07-30 18:01:10 -0700

    Improved robot_monitor. Still need to remove items dynamically from tree.

--- src/robot_dashboard/robot_monitor.py
+++ src/robot_dashboard/robot_monitor.py
@@ -6,6 +6,58 @@ from QtGui import QWidget, QVBoxLayout, QTreeWidget, QTextCursor, QTreeWidgetIte
 
 from QtCore import pyqtSignal
 
+def get_nice_name(status_name):
+    return status_name.split('/')[-1]
+
+def remove_parent_name(status_name):
+    return ('/'.join(status_name.split('/')[2:])).strip()
+
+def get_parent_name(status_name):
+    return ('/'.join(status_name.split('/')[:-1])).strip()
+
+class StatusItem(QTreeWidgetItem):
+    def __init__(self, status):
+        super(StatusItem, self).__init__()
+    
+        self.items = []
+        self.name = status.name
+        self.level = status.level
+        
+        self.setText(0, '/' + get_nice_name(self.name))
+
+    def get_children(self, msg):
+        ret = []
+
+        for k in msg.status:
+            if k.name.startswith(self.name):
+                if not k.name == self.name:
+                    ret.append(k)
+
+        return ret
+
+    def update(self, level, msg):
+        self.level = level
+        
+        children = self.get_children(msg)
+
+        names = [s.name for s in self.items]
+        new_items = []
+        remove = []
+        for i in children:
+            name = i.name
+            if name in names:
+                w = self.items[names.index(name)]
+                w.update(i.level, msg)
+            elif len(self.strip_child(name).split('/')) <= 2:
+                sti = StatusItem(i)
+                sti.update(i.level, msg)
+                self.items.append(sti)
+                new_items.append(sti)
+        self.addChildren(new_items)
+
+    def strip_child(self, child):
+        return child.replace(self.name, '')
+
 class RobotMonitor(QWidget):
     sig_err = pyqtSignal(str)
     sig_warn = pyqtSignal(str)
@@ -13,6 +65,7 @@ class RobotMonitor(QWidget):
 
     def __init__(self, topic):
         super(RobotMonitor, self).__init__()
+        self.top_items = []
         layout = QVBoxLayout()
 
         self.err = QTreeWidget()
@@ -37,12 +90,41 @@ class RobotMonitor(QWidget):
 
     def cb(self, msg):
         self.sig_clear.emit()
+        self.update_tree(msg)
+        self.update_we(msg)
+
+    def update_tree(self, msg):
+        #Update the tree from the bottom
+
+        names = [get_nice_name(k.name) for k in self.top_items]
+        add = []
+        for i in self._top_level(msg):
+            name = get_nice_name(i.name)
+            if name in names:
+                self.top_items[names.index(name)].update(i.level, msg)
+            else:
+                nw = StatusItem(i)
+                nw.update(i.level, msg)
+                self.top_items.append(nw)
+                add.append(nw)
+        
+        self.comp.addTopLevelItems(add)
+        
+    def _top_level(self, msg):
+        ret = []
+        for i in msg.status:
+            if len(i.name.split('/')) == 2:
+                ret.append(i)
+        
+        return ret
+
+    def update_we(self, msg):
         for status in msg.status:
             if status.level == status.WARN:
-                txt = "%s : %s"%(status.name, "Warning")
+                txt = "%s : %s"%(status.name, status.message)
                 self.sig_warn.emit(txt)
             elif status.level == status.ERROR:
-                txt = "%s : %s"%(status.name, "Error")
+                txt = "%s : %s"%(status.name, status.message)
                 self.sig_err.emit(txt)
 
     def clear(self):
